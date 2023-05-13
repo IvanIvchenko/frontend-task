b82f5224273aa57ae06d08faf6f3ad943f25dd48
IvanIvchenko <pavlo.oliinyk@olsysltd.com>
2023-05-13 18:11:24 +0300

    cleanup + ts fix

--- src/utils/helpers/filterHelpers.ts
+++
@@ -1,7 +0,0 @@
-export const transformArrayToObj = (arr: string[]): {[key: string]: boolean} => {
-  const obj: {[key: string]: boolean} = {};
-  arr.forEach((str) => {
-      obj[str] = false;
-  });
-  return obj;
-};
